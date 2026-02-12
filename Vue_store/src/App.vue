bef5549192213d8fccdaf2bedcb581508ecea20c
Vinsonc6 <vinsonc6@nycstudents.net>
2026-02-12 09:34:09 -0800

    e

--- Vue_store/src/App.vue
+++ Vue_store/src/App.vue
@@ -5,8 +5,17 @@
   </template>
 
   <script setup>
-Worst films in my opinion store[
-  
+const HorribleMovies = [
+{name: "Big Hero Six", yearReleased: 2014, price: 6.99,},
+{name: "Finding Dory", yearReleased: 2016, price: 6.99},
+{name: "Cars 3", yearReleaed: 2017, price: 5.99},
+{name: "Megamind vs. the Doom Syndicate", yearReleased: 2024, price: 8.00},
+{name: "Star Wars: The Rise of Skywalker", yearReleased: 2019,price: 7.99},
+{name: "Mufasa: The Lion King", yearReleased: 2024, price: 5.99},
+{name: "The Good Dinosaur", yearReleased: 2015, price: 2.99},
+{name: "Shark Tale", yearReleased: 2004, price: 5.99},
+{name: "The Hangover 3", yearReleased: 2013, price:6.99},
+{name: "Ratatoing"}
 ]
 </script>
 
