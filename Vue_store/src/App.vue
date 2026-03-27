f25bd940e680988a9b122577542b3ca306f13ce8
Vinsonc6 <vinsonc6@nycstudents.net>
2026-03-26 22:33:32 -0400

    e

--- Vue_store/src/App.vue
+++ Vue_store/src/App.vue
@@ -1,27 +1,26 @@
 <template>
-  <h1>Movies that I dislike so you can buy!</h1>
+  <div class="container">
 
-<routerView>
-  
-</routerView>
-
-<div class="container">
-</div id="nav">
-    
-<FilmsCard
-      v-for="film in films"
+    <CardComponent
+      v-for="film in HorribleFilms"
       :key="film.name"
-      :film="film"
+      :title="film.name"
+      :year="film.yearReleased"
+      :price="film.price"
+      image="https://picsum.photos/300/200"
     />
+
+  </div>
 </template>
 
 <script setup>
-import FilmsCard from '@/components/icons/Cards.vue'
+import { ref } from 'vue'
+import CardComponent from "./components/CardComponent.vue"
 
-const HorribleFilms = [
-{name: "Big Hero Six", yearReleased: 2014, price: 6.99,},
+const HorribleFilms = ref([
+{name: "Big Hero Six", yearReleased: 2014, price: 6.99},
 {name: "Finding Dory", yearReleased: 2016, price: 6.99},
-{name: "Cars 3", yearReleaed: 2017, price: 5.99},
+{name: "Cars 3", yearReleased: 2017, price: 5.99},
 {name: "Megamind vs. the Doom Syndicate", yearReleased: 2024, price: 8.00},
 {name: "Star Wars: The Rise of Skywalker", yearReleased: 2019,price: 7.99},
 {name: "Mufasa: The Lion King", yearReleased: 2024, price: 5.99},
@@ -30,9 +29,13 @@ const HorribleFilms = [
 {name: "The Hangover 3", yearReleased: 2013, price:6.99},
 {name: "Welcome to Derry", yearReleased: 2025, price:10.99},
 {name: "Daddy Doesn't Live Here Anymore", yearReleased:2022, price:3.99}
-]
+])
 </script>
 
-<style scoped>
-
+<style>
+.container{
+  display:flex;
+  flex-wrap:wrap;
+  gap:20px;
+}
 </style>
